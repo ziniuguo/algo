9374e79eb2fd0508e57279e35a0600f245ebb324
RagdollCatRoll <ziniu_guo@mymail.sutd.edu.sg>
2022-04-04 16:15:21 +0800

    Update leetcode66.java

--- leetcode66.java
+++ leetcode66.java
@@ -1,15 +1,26 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class leetcode66 {
-    public int[] plusOne(int[] digits) {
-        int l = digits.length;
-        if (digits[l - 1] != 9) {
-            digits[l - 1] = digits[l - 1] + 1;
-        } else {
-            digits[l-1]=0;
-            plusOne(digits [])\
-            digits.
+    public static int[] plusOne(int[] digits) {
+
+        int n = digits.length;
+        for(int i=n-1; i>=0; i--) {
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
         }
-        return digits;
+        int[] newNumber = new int [n+1];
+        newNumber[0] = 1;
+        return newNumber;
+    }
+
+    public static void main(String[] args) {
+        int[] testcase = new int[] {7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6};
+
+        System.out.println(Arrays.toString(plusOne(testcase)));
     }
 }
