892ec2e9a4ea964bf8a4f5f116eb8e736def0a47
RagdollCatRoll <ziniu_guo@mymail.sutd.edu.sg>
2022-04-04 13:06:45 +0800

    commit

---
+++ leetcode3.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+public class leetcode3 {
+    public static int lengthOfLongestSubstring(String s) {
+        int ans = 0;
+        for (int i=0; i<s.length(); i++){
+            System.out.println("i: "+i);
+            String st = "";
+            int subans = 0;
+            for (int j=i, jd=0; j<s.length() && jd == 0; j++) {
+                System.out.println("j: " + j);
+                char c = s.charAt(j);
+                System.out.println("char c: " + c);
+                if (st.indexOf(c) == -1) {
+
+                    st += c;
+                    System.out.println("inner " + st);
+                    subans += 1;
+                } else {
+                    jd = 1;
+                }
+            }
+            System.out.println(st);
+            if (subans > ans) {
+                ans = subans;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("wkj"));
+    }
+}
---
+++ leetcode66.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class leetcode66 {
+    public int[] plusOne(int[] digits) {
+        int l = digits.length;
+        if (digits[l - 1] != 9) {
+            digits[l - 1] = digits[l - 1] + 1;
+        } else {
+            digits[l-1]=0;
+            plusOne(digits [])\
+            digits.
+        }
+        return digits;
+    }
+}
