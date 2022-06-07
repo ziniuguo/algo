f227e3faffeed0a66d9d65a2ab936eb384ab9c63
Ziniu <guo.ziniu.1003@gmail.com>
2022-06-07 20:24:50 +0800

    leetcode209java

--- leetcode39.java
+++ leetcode39.java
@@ -4,55 +4,59 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-class myNode {
 
-    int val;
-    ArrayList<myNode> children;
+public class leetcode39 {
+    private class myNode {
 
-    myNode parent;
+        int val;
+        ArrayList<myNode> children;
 
-    myNode(int val) {
-        this.val = val;
-        this.children = new ArrayList<>();
-    }
-}
+        myNode parent;
 
-class Solution {
-    List<List<Integer>> ansList = new ArrayList<>();
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-
-        myNode rootNode = new myNode(target);
-        System.out.println(rootNode.children);
-        treeSetup(rootNode, candidates);
-        for (myNode i: rootNode.children) {
-            System.out.println(i.val);
+        myNode(int val) {
+            this.val = val;
+            this.children = new ArrayList<>();
         }
-        return ansList;
     }
 
-    public void treeSetup (myNode root, int[] candidates) {
-        for (int i: candidates) {
-            if (i <= root.val) {
-                myNode child = new myNode(root.val - i);
-                root.children.add(child);
-                child.parent = root;
+    class Solution {
+        List<List<Integer>> ansList = new ArrayList<>();
+        public List<List<Integer>> combinationSum(int[] candidates, int target) {
 
-                if (child.val == 0) {
-                    ArrayList<Integer> currAns = new ArrayList<>();
-                    while (child.parent != null) {
-                        currAns.add(child.parent.val - child.val);
-                        child = child.parent;
-                    }
-                    Collections.sort(currAns);
-                    if (!ansList.contains(currAns)) {
-                        ansList.add(currAns);
+            myNode rootNode = new myNode(target);
+            System.out.println(rootNode.children);
+            treeSetup(rootNode, candidates);
+            for (myNode i: rootNode.children) {
+                System.out.println(i.val);
+            }
+            return ansList;
+        }
 
+        public void treeSetup (myNode root, int[] candidates) {
+            for (int i: candidates) {
+                if (i <= root.val) {
+                    myNode child = new myNode(root.val - i);
+                    root.children.add(child);
+                    child.parent = root;
+
+                    if (child.val == 0) {
+                        ArrayList<Integer> currAns = new ArrayList<>();
+                        while (child.parent != null) {
+                            currAns.add(child.parent.val - child.val);
+                            child = child.parent;
+                        }
+                        Collections.sort(currAns);
+                        if (!ansList.contains(currAns)) {
+                            ansList.add(currAns);
+
+                        }
                     }
                 }
             }
-        }
-        for (myNode child: root.children) {
-            treeSetup(child, candidates);
+            for (myNode child: root.children) {
+                treeSetup(child, candidates);
+            }
         }
     }
+
 }
