44f8e3c732a955cd35d9c66081cbbd3e08aa85c3
ziniuguo <guo.ziniu.1003@gmail.com>
2023-05-01 20:58:19 +0800

    test: add testdrive class

---
+++ test.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class test {
+    // -1000, -3, 1, 3, 10    0
+    // -3000, -2000, -3, 1, 2   -2
+    // 1, 2, 3, 1000, 2000, 3000, 40000    5
+    public static void main(String[] args) {
+
+    }
+}
