5a030e65ab5d07e86b0a6ed5655194eb0ea97d46
ziniuguo <guo.ziniu.1003@gmail.com>
2023-05-05 02:41:10 +0800

    leetcode15 3sum sort pass

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -8,6 +10,18 @@ public class test {
     // -3000, -2000, -3, 1, 2   -2
     // 1, 2, 3, 1000, 2000, 3000, 40000    5
     public static void main(String[] args) {
-
+        ArrayList<Integer> testHashSet = new ArrayList<>();
+        Integer intObj = new Integer(1);
+        Integer intObj2 = new Integer(1);
+        testHashSet.add((int) intObj);
+        testHashSet.add((Integer)intObj2);
+        testHashSet.add((Integer)intObj2);
+        testHashSet.add(new Integer(2));
+        System.out.println(testHashSet);
     }
 }
+
+
+// a b c d e f g
+// a b c d e f g
+// a b c d e f g
