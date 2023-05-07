09fad7fd601e011ce0cfc0877f5245b01946511a
ziniuguo <guo.ziniu.1003@gmail.com>
2023-05-07 23:36:45 +0800

    test

--- test.java
+++ test.java
@@ -1,19 +1,20 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class test {
     // -1000, -3, 1, 3, 10    0
     // -3000, -2000, -3, 1, 2   -2
     // 1, 2, 3, 1000, 2000, 3000, 40000    5
     public static void main(String[] args) {
-        ArrayList<ListNode> pointerList =
-                new ArrayList<>(0);
-        System.out.println(pointerList);
-
+        int[] numsIn = new int[]{4, 9, 111, 12, 2};
+        swap(numsIn);
+        System.out.println(Arrays.toString(numsIn));
+    }
+    public static void swap(int[] nums) {
+        int temp = nums[1];
+        nums[1] = nums[0];
+        nums[0] = temp;
     }
 }
 
