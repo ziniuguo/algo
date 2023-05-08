c5097d54b978acf4b1a430b4fcd5e7232bbd22c1
ziniuguo <guo.ziniu.1003@gmail.com>
2023-05-08 13:51:48 +0800

    leetcode49 time complexity O(nmlogm) docs

--- LeetCode49.java
+++ LeetCode49.java
@@ -11,6 +11,12 @@ public class LeetCode49 {
         System.out.println(groupAnagrams(inStr));
     }
     public static List<List<String>> groupAnagrams(String[] strings) {
+        /*
+        * time complexity:
+        * O(nmlogm)
+        * where n is the total number of words
+        * and m is the length of each word
+        * */
         HashMap<String, List<String>> ansMap = new HashMap<>();
         for (String string : strings) {
             String sorted = sortString(string);
