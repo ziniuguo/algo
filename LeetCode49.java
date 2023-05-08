4e45cfd53a4f9c3ff37053a57da22e5d4d3c23d5
ziniuguo <guo.ziniu.1003@gmail.com>
2023-05-08 13:49:29 +0800

    leetcode49 O(nmlogm) solution

--- LeetCode49.java
+++ LeetCode49.java
@@ -1,4 +1,33 @@
 package leetcode;
 
+
+import java.util.*;
+
 public class LeetCode49 {
+    public static void main(String[] args) {
+        String[] inStr = new String[]{
+                "eat","tea","tan","ate","nat","bat"
+        };
+        System.out.println(groupAnagrams(inStr));
+    }
+    public static List<List<String>> groupAnagrams(String[] strings) {
+        HashMap<String, List<String>> ansMap = new HashMap<>();
+        for (String string : strings) {
+            String sorted = sortString(string);
+            if (ansMap.get(sorted) == null) {
+                LinkedList<String> currAnsList = new LinkedList<>();
+                currAnsList.add(string);
+                ansMap.put(sorted, currAnsList);
+            } else {
+                ansMap.get(sorted).add(string);
+            }
+        }
+        return new LinkedList<>(ansMap.values());
+    }
+    public static String sortString(String str) {
+        char[] res = str.toCharArray();
+        Arrays.sort(res);
+        return new String(res);
+//        return String.valueOf(res); valueOf is new String()
+    }
 }
