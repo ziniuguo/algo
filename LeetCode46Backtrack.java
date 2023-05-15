62bd9eb7b86b795f05feac6f268398b88261c1cc
ziniuguo <guo.ziniu.1003@gmail.com>
2023-05-15 21:02:02 +0800

    leetcode46 47

---
+++ LeetCode46Backtrack.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode46Backtrack {
+    public static void main(String[] args) {
+        int[] in = {1, 2, 3, 4};
+        System.out.println(permute(in));
+    }
+
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        backtrack(list, new ArrayList<>(), nums);
+        return list;
+    }
+
+    private static void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums){
+        if(tempList.size() == nums.length){
+            list.add(new ArrayList<>(tempList));
+        } else{
+            for (int num : nums) {
+                if (tempList.contains(num)) continue; // element already exists, skip
+                tempList.add(num);
+                backtrack(list, tempList, nums);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}
--- LeetCode47.java
+++ LeetCode47.java
@@ -1,2 +1,43 @@
-package leetcode;public class LeetCode47 {
+package leetcode;
+
+import java.util.*;
+
+public class LeetCode47 {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        // result list
+        HashMap<Integer, Integer> counter = new HashMap<>();
+        // to count the occurrence of a certain integer
+        for (int num : nums) {
+            if (!counter.containsKey(num)) {
+                counter.put(num, 1);
+            } else {
+                counter.put(num, counter.get(num) + 1);
+            }
+        }
+
+        LinkedList<Integer> comb = new LinkedList<>();
+        backtrack(comb, nums.length, counter, results);
+        return results;
+    }
+
+    private void backtrack(LinkedList<Integer> comb, Integer n,
+                           HashMap<Integer, Integer> counter,
+                           List<List<Integer>> results) {
+        if (comb.size() == n) {
+            // add copy of comb to results
+            results.add(new ArrayList<>(comb));
+            return;
+        }
+        for (Map.Entry<Integer, Integer> entry : counter.entrySet()) {
+            Integer num = entry.getKey();
+            Integer count = entry.getValue();
+            if (count == 0) continue;
+            comb.addLast(num);
+            counter.put(num, count - 1);
+            backtrack(comb, n, counter, results);
+            comb.removeLast();
+            counter.put(num, count);
+        }
+    }
 }
