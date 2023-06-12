a7b352da6f7e3a7b0f746535892933364fa921fe
ziniuguo <guo.ziniu.1003@gmail.com>
2023-06-12 15:46:55 +0800

    LeetCode1114 - Concurrency - busy waiting sol

--- src/main/java/io/catroll/algo/LeetCode1114.java
+++ src/main/java/io/catroll/algo/LeetCode1114.java
@@ -1,2 +1,35 @@
-package io.catroll.algo;public class LeetCode1114 {
+package io.catroll.algo;
+
+public class LeetCode1114 {
+    class Foo {
+        private boolean first = true;
+        private boolean second = true;
+
+
+        public Foo() {
+
+        }
+        public void first(Runnable printFirst) throws InterruptedException {
+            // printFirst.run() outputs "first". Do not change or remove this line.
+            printFirst.run();
+            first = false;
+        }
+        public void second(Runnable printSecond) throws InterruptedException {
+            // printSecond.run() outputs "second". Do not change or remove this line.
+            while (first) {
+                Thread.sleep(10);
+            }
+            printSecond.run();
+            second = false;
+        }
+
+        public void third(Runnable printThird) throws InterruptedException {
+            // printThird.run() outputs "third". Do not change or remove this line.
+            while (second) {
+                Thread.sleep(10);
+            }
+            printThird.run();
+        }
+    }
+
 }
