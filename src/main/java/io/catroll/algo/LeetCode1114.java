1a74abd3dac67c4829798f513a6001a11d20a284
Ziniu <guo.ziniu.1003@gmail.com>
2023-07-18 20:04:47 +0800

    LeetCode 1114: lock 2 objs

--- src/main/java/io/catroll/algo/LeetCode1114.java
+++ src/main/java/io/catroll/algo/LeetCode1114.java
@@ -73,4 +73,48 @@ public class LeetCode1114 {
             }
         }
     }
+
+    class Foo3 {
+        private final Object lock1;
+        private final Object lock2;
+        private boolean oneDone;
+        private boolean twoDone;
+
+        public Foo3() {
+            lock1 = new Object();
+            lock2 = new Object();
+            oneDone = false;
+            twoDone = false;
+        }
+
+        public void first(Runnable printFirst) throws InterruptedException {
+            synchronized (lock1) {
+                printFirst.run();
+                oneDone = true;
+                lock1.notifyAll();
+            }
+        }
+
+        public void second(Runnable printSecond) throws InterruptedException {
+            synchronized (lock1) { // bad practice?
+                synchronized (lock2) {
+                    while (!oneDone) {
+                        lock1.wait();
+                    }
+                    printSecond.run();
+                    twoDone = true;
+                    lock2.notifyAll();
+                }
+            }
+        }
+
+        public void third(Runnable printThird) throws InterruptedException {
+            synchronized (lock2) {
+                while (!twoDone) {
+                    lock2.wait();
+                }
+                printThird.run();
+            }
+        }
+    }
 }
