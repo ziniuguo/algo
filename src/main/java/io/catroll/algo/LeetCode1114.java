0361acc289812134706ab6f1aee8c18cdc5646f9
Ziniu <guo.ziniu.1003@gmail.com>
2023-07-19 20:30:13 +0800

    LeetCode1114: semaphore

--- src/main/java/io/catroll/algo/LeetCode1114.java
+++ src/main/java/io/catroll/algo/LeetCode1114.java
@@ -1,5 +1,7 @@
 package io.catroll.algo;
 
+import java.util.concurrent.Semaphore;
+
 public class LeetCode1114 {
     class Foo {
         private boolean oneDone;
@@ -115,6 +117,32 @@ public class LeetCode1114 {
                 }
                 printThird.run();
             }
+        }
+    }
+
+    class Foo4 {
+        private Semaphore s2;
+        private Semaphore s3;
+
+        public Foo4() {
+            s2 = new Semaphore(0);
+            s3 = new Semaphore(0);
+        }
+
+        public void first(Runnable printFirst) throws InterruptedException {
+            printFirst.run();
+            s2.release();
+        }
+
+        public void second(Runnable printSecond) throws InterruptedException {
+            s2.acquire();
+            printSecond.run();
+            s3.release();
+        }
+
+        public void third(Runnable printThird) throws InterruptedException {
+            s3.acquire();
+            printThird.run();
         }
     }
 }
